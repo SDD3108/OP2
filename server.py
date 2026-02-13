b00bb2f72da9e768e6b54c5278103b301b7b8222
SDD3108 <mrbimson1@gmail.com>
2026-02-13 20:20:33 +0500

    first commit

--- server.py
+++ server.py
@@ -1,1 +1,89 @@
-print('yo')
+# print('yo')
+# num = input('write ')
+# print(type (num))
+# print(num)
+# for i in num:
+#     print(type (i))
+
+# while - цикл
+
+# i = 1
+
+# while(i <= 5):
+#     print(i)
+#     i += 1
+
+
+# password = ''
+
+# while(password != '1234'):
+#     password = input("write password")
+# print('access')
+    
+
+# text = 'react'
+
+# print(text[0])
+# print(text[-1])
+# print(len(text))
+# print(text.lower())
+# print(text.upper())
+
+# text2 = '12345'
+# print(text2.isdigit())
+
+# new_text = text.replace('c','t')
+# print(new_text)
+
+# print(text[0:3])
+
+# print('tt' in new_text)
+
+# login = input('write login ')
+# if(login.isdigit()):
+#     print('login doesnt has to be only with numbers')
+# elif(len(login) > 5):
+#     print('too long login')
+# else:
+#     print('norm')
+
+# for i in range(3):
+#     for j in range(5):
+#         print(i,j)
+
+# for i in range(3):
+#     for j in range(5):
+#         for k in range(7):
+#             print(i,j,k)
+
+# квадрат короч
+# for i in range(4):
+#     for j in range(6):
+#         print('*',end='')
+#     print()
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print('*',end='')
+#     print()
+
+# num = ''
+# while(num != '0'):
+#     num = input('write ')
+
+# stroka = input('write ')
+# nums = 0
+# words = 0
+# for i in stroka:
+#     if(i.isdigit()):
+#         nums += 1
+#     else:
+#         words += 1
+# print(nums)
+# print(words)
+
+# for i in range(1,7):
+#     for j in range(i):
+#         print('*',end='')
+#     print()
+
